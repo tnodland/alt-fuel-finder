ac478985937817bc297b74e4edf08ab44292edb2
Josh Mejia <joshua.mejia@gmail.com>
2018-08-31 13:36:41 -0600

    Remove address attr_reader from Station

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,10 +1,9 @@
 class Station
-  attr_reader :name, :address, :fuel_types, :distance, :access_times
+  attr_reader :name, :fuel_types, :distance, :access_times
 
   def initialize(attrs = {})
     @attrs = attrs
     @name = attrs[:station_name]
-    @address = attrs[:street_address]
     @fuel_types = attrs[:fuel_type_code]
     @distance = attrs[:distance]
     @access_times = attrs[:access_days_time]
